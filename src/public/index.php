ad8ebed247c54d859fd2afeffdbb3753f2e045ae
Luke Roszmann <6002ekul@gmail.com>
2023-07-30 17:17:16 +0100

    Build utility

--- src/public/index.php
+++
@@ -1,9 +0,0 @@
-<?php
-$path = $_SERVER['REQUEST_URI'];
-define("SVELTE", "/../../svelte_app/public/");
-
-if ($path === '/') {
-    require __DIR__ . '/index.html';
-} else {
-    echo "404 - not found";
-}
